6a42d97077ce3839bab92cc651ba17c62f86db6e
Jose Garcia Sanchez <jgsanchez@lunia.es>
2021-10-11 15:12:36 +0200

    1 - Servicio de consulta de próximos lanzamientos

---
+++ src/app/core/space.service.spec.ts
@@ -0,0 +1,45 @@
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+  TestRequest,
+} from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
+import {SpaceService} from './space.service';
+
+fdescribe('GIVEN the ProjectsService isolated from remote server', () => {
+  let service: SpaceService;
+  let controller: HttpTestingController;
+  let inputBaseUrl: string;
+  const limit = 10;
+  const mode = '&mode=list';
+  beforeEach(() => {
+    inputBaseUrl = 'https://lldev.thespacedevs.com/2.0.0/launch/upcoming/';
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
+    service = TestBed.inject(SpaceService);
+    controller = TestBed.inject(HttpTestingController);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+  describe('WHEN getUpcomingLaunches$ is called', () => {
+    beforeEach(() => {
+      service.getUpcomingLaunches$().subscribe();
+    });
+    it('THEN it should call the right url', () => {
+      controller.expectOne(inputBaseUrl + '?limit=' + limit + mode);
+    });
+  });
+  describe('WHEN getUpcomingLaunches$ is called', () => {
+    let testRequest: TestRequest;
+    beforeEach(() => {
+      service.getUpcomingLaunches$(limit).subscribe();
+      testRequest = controller.expectOne(inputBaseUrl + '?limit=' + limit + mode);
+    });
+    it('THEN request should be sent by expected method', () => {
+      expect(testRequest.request.method).toBe('GET');
+    });
+  });
+});
